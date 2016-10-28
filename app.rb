3927aef5b4529d2867a7c9185569ab4648ba936b
Ihor <iwenchak@gmail.com>
2016-10-28 16:31:14 +0300

    Add validation

--- app.rb
+++ app.rb
@@ -6,6 +6,12 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:mycontact.db"
 
 class Contact < ActiveRecord::Base
+      validates :name, length:{minimum:4}
+	  validates :position, presence:true
+	  validates :email, presence:true
+	  validates :additional_information, length:{minimum:10} 
+	  validates :adress, presence:true
+	  validates :company, presence:true
 end
 
 
@@ -34,12 +40,20 @@ post '/contacts' do
 				 # Contact.create(:name => @name, :position => @position, :company => @company ,:adress => @adress, :currency => @currency, :email => @email, :additional_information => @additional_information)
 	             
                  # Третій спосіб форми запису в базу(використовуємо ActiveRecord):				 
+				 # save - автоматично запускає валідацію
+				 
 				 # connect = Contact.new params[:contact]
-                 # connect.save	
-                
+                 # connect.save
+				 
 				 # Четвертий спосіб форми запису в базу найкоротший(використовуємо ActiveRecord):
-                 Contact.create params[:contact]				
-				  
-				 erb :contact
-				  
-end
+                 # Contact.create params[:contact]				
+				 
+				 connect = Contact.new params[:contact]
+
+				 if connect.save					
+					erb :contact
+				 else
+				    @error_val = connect.errors.full_messages.first
+					erb :contact
+    			 end
+ end
