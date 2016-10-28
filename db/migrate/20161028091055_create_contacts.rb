940d383176827d602545241ae8373f34c93c5e2c
Ihor <iwenchak@gmail.com>
2016-10-28 12:21:33 +0300

    Create table

--- db/migrate/20161028091055_create_contacts.rb
+++ db/migrate/20161028091055_create_contacts.rb
@@ -1,4 +1,14 @@
 class CreateContacts < ActiveRecord::Migration
   def change
+     create_table:contacts  do |contact| 
+                  contac.text:tname
+				  contac.text:position
+				  contac.text:company
+				  contac.text:adress
+			      contac.text:currency
+			      contac.text:email
+			      contac.text:additional_information
+				  contact.timestamps
+	 end  
   end
 end
